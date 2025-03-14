ef84a6a2338ed5648b7688922196fe8c360469be
Jader Moura <jader.dev.moura@gmail.com>
2025-03-14 19:54:55 -0300

    fix: remove unused Link import

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, useEffect } from 'react'
-import { useNavigate, Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { jwtDecode } from 'jwt-decode'
 import Cookies from 'js-cookie'
 
